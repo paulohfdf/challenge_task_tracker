ca1c836ab59451ec4fc9d3e48ae3811acdf28546
paulohfdf <paulohfdf@gmail.com>
2025-07-29 23:12:40 -0300

    :construction: Add initial interface CLI

--- src/main.go
+++ src/main.go
@@ -1,6 +1,14 @@
+package main
+
 import (
-	"bufio"
 	"fmt"
-	"os"
 	"strings"
 )
+
+func main() {
+
+	fmt.Println(strings.Repeat("*", 80))
+	fmt.Println(strings.Repeat("-", 28) + " TASK TRACKER IN GOLANG " + strings.Repeat("-", 28))
+	fmt.Println("* Digit 'help' for list of commands")
+
+}
